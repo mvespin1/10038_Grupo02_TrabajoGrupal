ebd5220e4502714c328bae6ab241e7e267581f84
Ivonne Alexandra Jacome Sangoluisa <iajacome2@espe.edu.ec>
2023-06-20 02:34:01 -0500

    ajaxprocess

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 $('.carousel').carousel({
-	interval:3000,
-	pause:"hover"
+	interval: 3000,
+	pause: "hover"
 })
+//EJEMPLO XMLHttpRequest
+function loadCatImage() {
+	const catUrl = "https://www.nationalgeographic.com/animals/mammals/facts/domestic-cat";
+	const catContainer = document.getElementById("cat-container");
+	catContainer.innerHTML = `
+	  <img src="${catUrl}" alt="Cat Image">
+	  <button type="button" onclick="loadCatImage()">Get Another Cat Image</button>
+	`;
+  }
